c23326c0bc35942a1d84344676ac20c5abf4ef0b
Daniel Allen <dan@aeternagroup.net>
2026-07-16 15:01:42 +0100

    Update to schema version v6.13.0

--- outputs.tf
+++ outputs.tf
@@ -1,13 +1,13 @@
 output "actions_organization_secret_repositories_id" {
   description = "Map of id values across all actions_organization_secret_repositories, keyed the same as var.actions_organization_secret_repositories"
-  value       = { for k, v in github_actions_organization_secret_repository.actions_organization_secret_repositories : k => v.id }
+  value       = { for k, v in github_actions_organization_secret_repository.actions_organization_secret_repositories : k => v.id if v.id != null && length(v.id) > 0 }
 }
 output "actions_organization_secret_repositories_repository_id" {
   description = "Map of repository_id values across all actions_organization_secret_repositories, keyed the same as var.actions_organization_secret_repositories"
-  value       = { for k, v in github_actions_organization_secret_repository.actions_organization_secret_repositories : k => v.repository_id }
+  value       = { for k, v in github_actions_organization_secret_repository.actions_organization_secret_repositories : k => v.repository_id if v.repository_id != null }
 }
 output "actions_organization_secret_repositories_secret_name" {
   description = "Map of secret_name values across all actions_organization_secret_repositories, keyed the same as var.actions_organization_secret_repositories"
-  value       = { for k, v in github_actions_organization_secret_repository.actions_organization_secret_repositories : k => v.secret_name }
+  value       = { for k, v in github_actions_organization_secret_repository.actions_organization_secret_repositories : k => v.secret_name if v.secret_name != null && length(v.secret_name) > 0 }
 }
 
